bf53ee950c58e88f1e63f3d663aed7a5becde4e1
luowei729 <1084605775@qq.com>
2026-06-06 20:50:11 +0800

    update

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { createResponsesProxyHandler, createChatCompletionsProxyHandler, getRuntimeDefaultModel, setRuntimeDefaultModel, isAnthropicModel } = require('./proxy');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30001;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
